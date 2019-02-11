8997b902b08b99910ff4d3e0a167750c41c76158
Sergio Agostinho <sergio.agostinho@tecnico.ulisboa.pt>
2019-02-11 18:47:20 +0000

    Now it runs properly. Results still look crappy for the translation component.

--- estimator.py
+++ estimator.py
@@ -10,6 +10,41 @@ from .lib.loss_refiner import Loss_refine
 
 _PREFIX = os.path.dirname(os.path.realpath(__file__))
 
+def _quatm(q1, q0):
+    """Return multiplication of two quaternions.
+
+    >>> q = _quatm([4, 1, -2, 3], [8, -5, 6, 7])
+    >>> numpy.allclose(q, [28, -44, -14, 48])
+    True
+
+    """
+    w0, x0, y0, z0 = q0
+    w1, x1, y1, z1 = q1
+
+    return torch.cuda.FloatTensor([
+        -x1*x0 - y1*y0 - z1*z0 + w1*w0,
+        x1*w0 + y1*z0 - z1*y0 + w1*x0,
+        -x1*z0 + y1*w0 + z1*x0 + w1*y0,
+        x1*y0 - y1*x0 + z1*w0 + w1*z0,
+    ])
+
+
+def _update_pose(pose_original, update):
+
+    q, t = pose_original
+    qu, tu = update
+
+    quc = torch.cuda.FloatTensor([qu[0], -qu[1], -qu[2], -qu[3]])
+    q_final = _quatm(qu, _quatm(q, quc))
+
+    tq = torch.cuda.FloatTensor([0, t[0], t[1], t[2]])
+    tuq = torch.cuda.FloatTensor([0, tu[0], tu[1], tu[2]])
+    t_final = _quatm(qu, _quatm(tq, quc)) + tuq
+
+    return q_final, t_final[1:]
+
+
+
 class DenseFusionEstimator:
     """
     Warning: currently specialized for Linemod only.
@@ -42,37 +77,7 @@ class DenseFusionEstimator:
         self.criterion = Loss(self.num_points, self.sym_list)
         self.criterion_refine = Loss_refine(self.num_points, self.sym_list)
 
-    @staticmethod
-    def _quatm(q1, q0):
-        """Return multiplication of two quaternions.
 
-        >>> q = _quatm([4, 1, -2, 3], [8, -5, 6, 7])
-        >>> numpy.allclose(q, [28, -44, -14, 48])
-        True
-
-        """
-        w0, x0, y0, z0 = q0
-        w1, x1, y1, z1 = q1
-        return torch.Tensor([
-            -x1*x0 - y1*y0 - z1*z0 + w1*w0,
-            x1*w0 + y1*z0 - z1*y0 + w1*x0,
-            -x1*z0 + y1*w0 + z1*x0 + w1*y0,
-            x1*y0 - y1*x0 + z1*w0 + w1*z0])
-
-    @staticmethod
-    def _update_pose(pose_original, update):
-
-        q, t = pose_original
-        qu, tu = update
-
-        quc = torch.Tensor([qu[0], -qu[1], -qu[2], -qu[3]])
-        q_final = self._quatm(qu, self._quatm(q, quc))
-
-        tq = torch.Tensor([0, t[0], t[1], t[2]])
-        tuq = torch.Tensor([0, tu[0], tu[1], tu[2]])
-        t_final = self._quatm(qu, self._quatm(tq, quc)) + tuq
-
-        return q_final, t_final[1:]
 
 
     # def estimate(self, points, choose, img, target, model_points, idx):
@@ -89,13 +94,19 @@ class DenseFusionEstimator:
         _, _, new_points, new_target, max_idx = self.criterion(pred_r, pred_t, pred_c, target_, model_points_, idx_, points_, 0.0, False)
 
         # flatten things
+        # import pdb; pdb.set_trace()
         pred_r = pred_r[0, max_idx]
-        pred_t = pred_t[max_idx, 0]
+        pred_t = pred_t[0, max_idx]
 
-        for _ in range(self.iteration):
+        # for _ in range(self.iteration):
+        for _ in range(10):
             pred_r_inc, pred_t_inc = self.refiner(new_points, emb, idx_)
+            pred_r_inc = pred_r_inc.view(-1)
+            pred_t_inc = pred_t_inc.view(-1)
+
             # TODO Needs to be validated
-            pred_r, pred_t = self._update_pose((pred_r, pred_t), (pred_r_inc, pred_t_inc))
+            # import pdb; pdb.set_trace()
+            pred_r, pred_t = _update_pose((pred_r, pred_t), (pred_r_inc, pred_t_inc))
             _, new_points, new_target = self.criterion_refine(pred_r_inc, pred_t_inc, new_target, model_points_, idx_, new_points)
 
         return pred_r, pred_t, pred_c[0, max_idx]
