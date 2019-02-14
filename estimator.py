0cf376bba06db3a8b979a4c39eb4e866ee8caaba
Sérgio Agostinho <sergio.r.agostinho@gmail.com>
2019-02-14 14:52:24 +0000

    Revert iterator loop runs to original value

--- estimator.py
+++ estimator.py
@@ -98,8 +98,8 @@ class DenseFusionEstimator:
         pred_r = pred_r[0, max_idx]
         pred_t = pred_t[0, max_idx]
 
-        # for _ in range(self.iteration):
-        for _ in range(10):
+        for _ in range(self.iteration):
+        # for _ in range(10):
             pred_r_inc, pred_t_inc = self.refiner(new_points, emb, idx_)
             pred_r_inc = pred_r_inc.view(-1)
             pred_t_inc = pred_t_inc.view(-1)
