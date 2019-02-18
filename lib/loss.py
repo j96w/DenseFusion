0c4cde92c43ec11ac5f51e83da6db7e3f2ae16f7
mcarletti <marco.carletti@univr.it>
2019-02-18 17:37:24 +0100

    fix memory leak: force to instantiated and delete KNN each time loss function is invoked

--- lib/loss.py
+++ lib/loss.py
@@ -8,7 +8,6 @@ import random
 import torch.backends.cudnn as cudnn
 from .knn import KNearestNeighbor
 
-knn = KNearestNeighbor(1)
 
 def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, w, refine, num_point_mesh, sym_list):
     bs, num_p, _ = pred_c.size()
@@ -40,12 +39,14 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
 
     if not refine:
         if idx[0].item() in sym_list:
+            knn = KNearestNeighbor(1)
             target = target[0].transpose(1, 0).contiguous().view(3, -1)
             pred = pred.permute(2, 0, 1).contiguous().view(3, -1)
             inds = knn(target.unsqueeze(0), pred.unsqueeze(0))
             target = torch.index_select(target, 1, inds.view(-1) - 1)
             target = target.view(3, bs * num_p, num_point_mesh).permute(1, 2, 0).contiguous()
             pred = pred.view(3, bs * num_p, num_point_mesh).permute(1, 2, 0).contiguous()
+            del knn
 
     dis = torch.mean(torch.norm((pred - target), dim=2), dim=1)
     loss = torch.mean((dis * pred_c - w * torch.log(pred_c)), dim=0)
